2493da5155ac403bba7966f41743eb286c6ce1bd
HKCODES7 <hkjupyter@gmail.com>
2026-09-01 01:50:18 +0530

    Update svelte.config.js

--- apps/recipe-app/svelte.config.js
+++ apps/recipe-app/svelte.config.js
@@ -1,1 +1,1 @@
-import adapter from '@sveltejs/adapter-static'; import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'; export default {preprocess:vitePreprocess(),kit:{adapter:adapter({fallback:'200.html'})}};
+import adapter from '@sveltejs/adapter-static'; import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'; export default {preprocess:vitePreprocess(),kit:{adapter:adapter({fallback:'index.html'})}};
